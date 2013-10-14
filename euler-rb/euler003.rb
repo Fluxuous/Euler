918194b1c9ad79562f9fc487659ad81bd25cb3e4
Fluxuous <parkerferguson@gmail.com>
2013-10-14 09:57:15 -0400

    Update euler003.rb

--- euler-rb/euler003.rb
+++ euler-rb/euler003.rb
@@ -8,7 +8,7 @@ What is the largest prime factor of the number 600851475143 ?
 def prime_factors(n) 
 
   return [] if n == 1      
-  factor = (2..n).find {|i| n % i == 0}       #Find factors and store value for any number given. Store the value. 
-  [factor] + prime_factors(n / factor)        #Find the largest prime factor any the given number 
+  factor = (2..n).find {|i| n % i == 0}       
+  [factor] + prime_factors(n / factor)        
 end
 puts prime_factors(600851475143).max
