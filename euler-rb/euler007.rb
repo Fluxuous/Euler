20206cb649f882d22689b6abba0c5c76ea1509f3
Fluxuous <parkerferguson@gmail.com>
2013-10-14 12:55:12 -0400

    Update euler007.rb

--- euler-rb/euler007.rb
+++ euler-rb/euler007.rb
@@ -3,5 +3,5 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10001st prime number?
 =end
 
-require 'mathn'					# Lazy access to Prime number
-puts Prime.take(10001).last     # Easy Grab and print of the 10001th Prime
+require 'mathn'				
+puts Prime.take(10001).last
